0f120bb89304b3c7c55c1a3701a68111ea19c5e6
havishshetty <havish.mitmpl2022@learner.manipal.edu>
2024-05-26 18:53:32 +0400

    LinkedList

--- linkedlist.java
+++ linkedlist.java
@@ -76,6 +76,113 @@ public class linkedlist {
         return sizeofll;
 
     }
+    public static int removefirst(){
+        if(head==null){
+            return -1;
+        }
+        int val=head.data;
+        head=head.next;
+        return val;
+    }
+
+    public static void removelast(){
+        if(head==null){
+            return ;
+        }
+        Node prev=head;
+        while(prev.next!=null){
+            prev=prev.next;
+        }
+        prev.next=null;
+        tail=prev;
+    }
+
+    public static void itersearch(int val){
+        Node temp=head;
+        int flag=0;
+        while(temp!=null){
+            if(temp.data==val){
+                flag=1;
+            }
+            temp=temp.next;
+        }
+    }
+    public static void reversell(){
+        Node prev=null;
+        Node curr=head;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+
+    public void deletenthfromend(int n){
+        //first find size
+        int size=0;
+        Node curr=head;
+        while(curr!=null){
+            curr=curr.next;
+            size++;
+        }
+        if(n==size){//first node
+            head=head.next;
+
+        }
+        Node temp=head;
+        int key=size-n;
+        int i=0;
+        while(i<key){
+            i++;
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
+        return;
+    }
+
+    public static boolean isPalin(){
+        //find middle node using slow fast concept(helper function)
+        //then divide into two ll
+        //if the reverse of second half is equal to first half then true
+        if(head==null ||  head.next==null){
+            return true;
+        }
+        Node middle=helper(head);
+
+        Node curr=middle;
+        Node prev=null;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+
+        }
+        Node right=prev;
+        Node left=head;
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            right=right.next;
+            left=left.next;
+        }
+        return true;
+    }
+
+    public static Node helper(Node head){
+        Node fast=head;
+        Node slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
 
     
     
